89edd00d225f2dc889832959d01c181e2bf3a5e3
Imane Bendou <ibendou@andrew.cmu.edu>
2019-03-22 12:29:02 +0000

    Added routes for the methods of authors

--- lab6/config/routes.rb
+++ lab6/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Generated model routes
   resources :books
   # need routes for authors and categories ...
-  
+  resources :author
   # Additional routes we've created for this project
   get 'proposed' => 'books#proposed', as: :proposed_books
   get 'contracted' => 'books#contracted', as: :contracted_books
