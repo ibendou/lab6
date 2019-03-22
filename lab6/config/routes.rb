7c4bbda8da88961a5697c64836918aa1e75b78df
Imane Bendou <ibendou@andrew.cmu.edu>
2019-03-22 13:48:08 +0000

    Added 7 restful methods for categories

--- lab6/config/routes.rb
+++ lab6/config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   # Generated model routes
   resources :books
   # need routes for authors and categories ...
-  resources :author
+  resources :authors
+  resources :categories
+  
   # Additional routes we've created for this project
   get 'proposed' => 'books#proposed', as: :proposed_books
   get 'contracted' => 'books#contracted', as: :contracted_books
